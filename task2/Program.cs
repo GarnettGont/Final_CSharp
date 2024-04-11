5e65304e34bcf054e2e3167a32b7c3cd63ca29c6
garnettgont <garnettgont@gmail.com>
2024-04-11 21:00:54 +0300

    Task 2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Функция Аккермана
+
+int AckermannFun(int m, int n)
+{
+
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return AckermannFun(1, n - 1);
+    }
+    else
+    {
+        return AckermannFun(AckermannFun(m - 1, n), n - 1);
+    }
+
+}
+
+Console.Write("Write first not negative number: ");
+int numM = Convert.ToInt32(Console.ReadLine()); ;
+Console.Write("Write second not negative number: ");
+int numN = Convert.ToInt32(Console.ReadLine()); ;
+Console.WriteLine(AckermannFun(numM, numN));
